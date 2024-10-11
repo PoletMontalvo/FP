76f6f9a56f0932b540ef6f14ef4bb789b966bdc2
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-10-10 20:28:56 -0600

    Actualizar Código C++.cpp&

---
+++ Código C++.cpp&/Practica A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+// SECCIÓN DECLARACIÓN DE VARIABLES
+
+int materias = 5;
+cout << "Materias que estás cursado: " << materias << endl;
+
+int cuatrimestre = 6;
+cout << "Valor del cuatrimestre: " << cuatrimestre << endl;
+
+int numero = 335013822;
+cout << "Tu número de cuenta es: " << numero << endl;
+
+string materia = "fundamentos de programación. ";
+cout << "El nombre de la materia es: " << materia << endl;
+
+
+
+
+// SECCIÓN ANÁLISIS DE ERROR DE SINTAXIS 
+
+ 
+
+
+
+
+}
