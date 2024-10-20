1bbf99fe3d35629cbd2720d1d96d10f25570b846
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-10-19 20:04:45 -0600

    Actualizar If... else.cpp

--- Código C++.cpp&/If... else.cpp
+++ Código C++.cpp&/If... else.cpp
@@ -23,18 +23,19 @@ int main() {
 	}
 	return 0;
 
-	// 2. Encontrar el mayor de dos números, solicita los dos números de tipo int.
- int num1; 
- int num2;
+	
+        // 2. Encontrar el mayor de dos números, solicita los dos números de tipo int.
+        int num1; 
+        int num2;
  cout << "Dame el primero valor: ";
  cin >> num1;
- cout << "Dame el segundo valor: "
- cin >> num 2;
+ cout << "Dame el segundo valor: ";
+ cin >> num2;
  cout << endl;
- if (num1 < num2)
- { cout << "El mayor es: " << num1 << endl; }
- else (num2 < num1)
- { cout << "El mayor es: " << num2 << endl;}
+ if (num1 > num2)
+ {cout << "El mayor es: " << num1 << endl;}
+ else 
+ {cout << "El mayor es: " << num2 << endl;}
 
-return 0
-	]
+return 0;
+        }
