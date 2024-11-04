11d5b6b2d997866252771123d6e1f65ffbbd3c6a
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-11-04 11:24:29 -0600

    Update Practica D.cpp

--- Código C++.cpp&/Practica D.cpp
+++ Código C++.cpp&/Practica D.cpp
@@ -1,31 +1,30 @@
 
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
 	// 1.16 Diseñar un programa que lea tres números A, B, C y visualice en pantalla el valor del más grande.
-	int a, b, c;
-	cout << "A: ";
-	cin >> a;
-	cout << "B: ";
-	cin >> b;
-	cout << "C: ";
-	cin >> c;
+	int a = 1, b = 10, c = 100;
+	cout << "A: " << a << endl;
+	cout << "B: " << b << endl;;
+	cout << "C: " << c << endl;
 	if ((a > b) and (a > c))
 	{
-		cout << "El valor más grande es: " << a << endl;
+		cout << "El valor más grande es A " << endl;
 	}
 	if ((b > c) and (b > a))
 	{
-		cout << "El valor más grande es: " << b << endl;
+		cout << "El valor más grande es B " << endl;
 	}
 	if ((c > a) and (c > b))
 	{
-		cout << "El valor más grande es: " << c << endl;
+		cout << "El valor más grande es C" << endl;
 	}
 
 	// 1.17 Crear un programa que lea tres números diferentes, A, B, C, e imprime los valores máximo y mínimo. El procedimiento constistirá en comparaciones sucesivas de parejas de números. Cree el algoritmo, diagrama de flujo y código en C#
+	int a, b, c;
 
 
 	// 1.18 Crear un programa al que le solicitemos la HH, MM, SS y nos calcule la hora dentro de un segundo más tarde. Leeremos las horas, minutos, segundos como números enteros.
@@ -36,10 +35,18 @@ int main() {
 	cin >> mm;
 	cout << "SS: ";
 	cin >> ss;
-	cout << "La hora un segundo más tarde: " << hh << ":" << mm << ":" << (ss + 1) << endl;
+	if (mm < 59)
+	{
+		cout << "La hora un segundo más tarde: " << hh << ":" << mm << ":" << (ss + 1) << endl;
+	}
+	if (mm >= 59)
+	{
+		cout << "La hora un segundo más tarde: " << hh << ":" << (mm + 1) << ":" << ss << endl;
+	}
+
 
 	// 1.19 Determinar el precio del boleto de ida y vuelta en avión, conociendo la distancia a recorrer y sabiendo que si el número de días de estancia es superior a 7 y la distancia superior a 800 hm el billete tiene una reducción del 30%. El precio por km es de 2.5 dólares.
-	
+
 
 	// 1. 20 Escribir las instrucciones apropiadas para cada una de las siguientes condiciones:
 	  // a) Si un ángulo es igual de 90°
@@ -55,7 +62,7 @@ int main() {
 		cout << "El ángulo no es un ángulo recto. " << endl;
 	}
 
-	  // b) Si la temperatura es igual es superior a 100°
+	// b) Si la temperatura es igual es superior a 100°
 	double temperatura;
 	cout << "Temperatura: ";
 	cin >> temperatura;
@@ -67,13 +74,13 @@ int main() {
 	{
 		cout << "Por debajo del punto de ebullición del agua." << endl;
 	}
-	  // c) Recibir un número
+	// c) Recibir un número
 	int num;
 	cout << "Dame un número: ";
 	cin >> num;
-	
-	  // d) Si "x" es mayor que "y" y "z" es menor que "y", imprimir x.
-	  // e)
+
+	// d) Si "x" es mayor que "y" y "z" es menor que "y", imprimir x.
+	// e)
 
 	return 0;
 }
