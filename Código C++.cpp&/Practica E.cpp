5a3ff5b579381d1cfa26832f96bd550daf5cdd38
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-11-05 19:26:58 -0600

    Update Practica E.cpp

--- Código C++.cpp&/Practica E.cpp
+++ Código C++.cpp&/Practica E.cpp
@@ -15,7 +15,20 @@ int main (){
     cout << "Has salido del ciclo." << endl;
     
     // 1.22
-    
+        numero1 = 0;
+        numero2 = 0;
+        total = 0;
+        do 
+        {
+            cout << "Menú de operaciones aritméticas" << endl;
+            cout << "a) Suma de los operadores" << endl;
+            cout << "b) Resta de dos operadores" << endl;
+            cout << "c) Multiplicación de dos operadores" << endl;
+            cout << "d) División de dos operadores" << endl;
+            cout << "e) Residuo de dos operadores" << endl;
+            cout << "s) Salir del Menú" << endl;
+            cin >> opcion;
+        } while (opcion != 's');
     
     return 0;
 }
