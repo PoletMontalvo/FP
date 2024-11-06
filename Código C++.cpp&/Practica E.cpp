ecbef8d2c721bf184a22c1a548917905c29a133f
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-11-05 19:13:56 -0600

    Create Practica E.cpp

---
+++ Código C++.cpp&/Practica E.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+    
+    //1.21
+    string respuesta; 
+    do 
+    { 
+        cout << "Quieres salir del ciclo? [SI/NO]: "; 
+        cin >> respuesta; 
+    } while (respuesta != "SI"); 
+    cout << "Has salido del ciclo." << endl;
+    
+    // 1.22
+    
+    
+    return 0;
+}
