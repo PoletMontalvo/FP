10098caef5ff6c9bcb0d23fde97a10a4710876f8
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-11-14 12:03:03 -0600

    Create Practica G.cpp

---
+++ Código C++.cpp&/Practica G.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // 1. and 2.
+    string nombreMes[12]{ "Enero ","Febrebro ", "Marzo ", "Abril ", "Mayo ", "Junio ", "Julio ", "Agosto ", "Septiembre ", "Octubre ", "Noviembre ", "Diciembre " };
+    for (int i = 0; i < 12; i++)
+    {
+        cout << nombreMes[i] << endl;
+    }
+    cout << endl;
+
+    // 3. and 4.
+    string planetas[8]{ "Mercurio ", "Venus ", "Tierra ", "Marte ", "Júpiter ", "Saturno ", "Urano ", "Neptuno " };
+    for (int i = 0; i < 8; i++)
+    {
+        cout << planetas[i] << endl;
+    }
+    cout << endl;
+}
