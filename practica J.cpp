ecbd75730cec91e0c085e26ea215e289e4c3b15e
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-11-25 11:47:38 -0600

    Update practica J.cpp

--- practica J.cpp
+++ practica J.cpp
@@ -41,4 +41,19 @@ int main()
     {
         cout << elemento << endl;
     }
+
+    // 2.-
+    int numeros[10]
+{ 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+vector <int> vnum
+{ 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+int mayor = -1;
+for (int i = 0; i <= 9; i++)
+{
+    if (vnum[i] > mayor)
+    {
+        mayor = numeros[i];
+    }
+}
+cout << "El mayor es: " << mayor << endl;
 }
