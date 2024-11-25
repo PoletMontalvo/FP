7ced3eb56ab976774d8e533b96fb499874f51bd8
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-11-25 12:32:42 -0600

    Update practica J.cpp

--- practica J.cpp
+++ practica J.cpp
@@ -57,3 +57,61 @@ for (int i = 0; i <= 9; i++)
 }
 cout << "El mayor es: " << mayor << endl;
 }
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int numeros[10]
+    { 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+    int numero, primero, segundo, tercero, 
+        temp = 0;
+    primero = numeros[0];
+    segundo = numeros[1];
+    tercero = numeros[2];
+    if (primero < segundo)
+    {
+        temp = primero;
+        primero = segundo;
+        segundo = temp;
+    }
+    if (primero < tercero)
+    {
+        temp = primero;
+        primero = tercero;
+        tercero = temp;
+    }
+    if (segundo < tercero)
+    {
+        temp = segundo;
+        segundo = tercero;
+        tercero = temp;
+    }
+    for (int i = 3; i <= 9; i++)
+    {
+        if (numeros[i] > primero)
+        {
+            tercero = segundo;
+            segundo = primero;
+            primero = numeros[i];
+        }
+        else if
+            (numeros[i] > segundo)
+        {
+            tercero = segundo;
+            segundo = numeros[i];
+        }
+        else if
+            (numeros[i] > tercero)
+        {
+            tercero = numeros[i];
+        }
+    }
+    cout << primero << endl;
+    cout << segundo << endl;
+    cout << tercero << endl;
+}
+
