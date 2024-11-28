19752c4b1378117561819bfe957bdb6e144ea004
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-11-28 11:50:46 -0600

    Update Funciones.cpp

--- Código C++.cpp&/Funciones.cpp
+++ Código C++.cpp&/Funciones.cpp
@@ -21,3 +21,28 @@ int main() //Punto de entrada
     //hola(); //Esto es invocar o llamar la funcion
     suma();
 }
+
+#include <iostream>
+using namespace std;
+
+// variables globales: fuera de la funcion main
+int n1, n2;
+//funciones
+void pedirNumeros()
+{
+    cout << "Dame el no. 1: ";
+    cin >> n1;
+    cout << "Dame el no. 2: ";
+    cin >> n2;
+}
+void suma(int n1, int n2) //variable local
+{
+    //Suma con parametros
+    cout << n1 + n2 << endl;
+}
+//No puedo definir funciones dentro de otra funcion
+int main() //Punto de entrada
+{
+    pedirNumeros();
+    suma(n1, n2); //argumentos
+}
