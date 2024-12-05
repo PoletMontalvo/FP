5d15dfff9628c904a043aa889ed08bf040beb0f6
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-12-05 12:23:53 -0600

    Create clase.cpp

---
+++ clase.cpp
@@ -0,0 +1,119 @@
+
+#include <iostream>
+
+using namespace std;
+//Clase, se delcaran fuera de cualquier función
+class Enemigo
+{
+public:
+    string tipo;
+    int vida;
+    int potencia;
+    //Constructor enemigo
+    Enemigo(string tipo1, int vida1, int potencia1)
+    {
+        this->tipo = tipo1;
+        this->vida = vida1;
+        this->potencia = potencia1;
+    }
+    string getTipo()const
+    {
+        return tipo;
+    }
+    int getVida()const
+    {
+        return vida;
+    }
+    int getPotencia()const
+    {
+        return potencia;
+    }
+    void setTipo(string tipo1)
+    {
+        this->tipo = tipo1;
+    }
+    void setVida(int vida1)
+    {
+        this->vida = vida1;
+    }
+    void setPotencia(int potencia1)
+    {
+        this->potencia = potencia1;
+    }
+};
+//Subclase
+class Orco :public Enemigo
+{
+public:
+    Orco(int vida1, int potencia1):
+        Enemigo("Orco", vida1, potencia1)
+    {}
+};
+class Dragon :public Enemigo
+{
+public:
+    Dragon(int vida1, int potencia1):
+        Enemigo("Dragon", vida1, potencia1)
+    {}
+};
+class Goblin :public Enemigo
+{
+    Goblin(int vida1, int potencia1):
+        Enemigo("Goblin", vida1, potencia1)
+    {}
+};
+
+class Jugador 
+{
+public: 
+    string id; //Identificador del jugador
+    int vida;
+    int potencia;
+    // Un constructor genera un objeto o 
+    // una instancia de una clase
+    //Un constructor tiene el nombre de la clase
+    Jugador(string id1, int vida1, int potencia1) 
+    {
+        this->id = id1;
+        this->vida = vida1;
+        this->potencia = potencia1;
+    }
+    string getId()const
+    {
+        return id;
+    }
+    int getVida()const
+    {
+        return vida;
+    }
+    int getPotencia()const
+    {
+        return potencia;
+    }
+    //Setter
+    void setId(string id1)
+    {
+        this->id = id1;
+    }
+    void setVida(int vida1)
+    {
+        this->vida = vida1;
+    }
+    void setPotencia(int potencia1)
+    {
+        this->potencia = potencia1;
+    }
+    //Función prototipo, primera línea
+    //de la funcion terminando con ;
+    void ataque(Enemigo & enemigo);
+};
+
+
+int main()
+{
+    Jugador link("Link ",100,20);
+    cout << "Nombre: " << link.getId() << endl;
+    cout << "Vida: " << link.getVida() << endl;
+    cout << "Potencia: " << link.getPotencia() << endl;
+
+}
