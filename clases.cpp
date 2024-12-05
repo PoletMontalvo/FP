ca0edea0c367b289108c5f94ca6df2a951c4b376
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2024-12-05 11:22:44 -0600

    Update clases.cpp

--- clases.cpp
+++ clases.cpp
@@ -29,3 +29,52 @@ int main()
     cout << "Cuenta: " << polet.cuenta << endl;
 
 }
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+struct Alumno 
+{
+    // Atributos o propiedades
+public:
+    string nombre;
+    int cuenta;
+    //Funciones de la estructura
+public:
+    static void altaAlumno(vector<Alumno> & v1)
+    {
+        Alumno alumno;
+        cout << "Dame tu nombre: ";
+        cin >> alumno.nombre; 
+        cout << "Dame tu cuenta: ";
+        cin >> alumno.cuenta;
+        v1.push_back(alumno);
+    }
+
+    static void imprimeDatos(vector<Alumno> v1)
+    {
+        cout << "Tamano del vector: " << v1.size() << endl;
+        cout << endl;
+        for (int i = 0; i < v1.size(); i++)
+        {
+            cout << "nombre: " << v1[i].nombre << endl;
+            cout << "Cuenta: " << v1[i].cuenta << endl;
+        }
+    }
+
+};
+
+int main()
+{
+    vector<Alumno> v;
+    int numAlumnos;
+    cout << "Num de alumnos a dar de alta: ";
+    cin >> numAlumnos;
+    for (int i = 0; i < numAlumnos; i++)
+    {
+        Alumno::altaAlumno(v);
+    }
+    Alumno::imprimeDatos(v);
+}
