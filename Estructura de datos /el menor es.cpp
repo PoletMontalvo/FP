249214d59fa8793efe340afa563e25e54eaa2423
PoletMontalvo <polet.montalvo35@my.unitec.edu.mx>
2025-01-24 11:52:02 -0600

    Update el menor es.cpp

--- Estructura de datos /el menor es.cpp
+++ Estructura de datos /el menor es.cpp
@@ -2,12 +2,21 @@
 #include <iostream>
 using namespace std;
 
+/*Variable global se crea cuando comienza la 
+ejecucion, se pierde cuando finaliza el programa.*/
+
 //Prototipo de funcion: para llamar a la funcion 
 int MinBetweenTwoNumbers(int n1, int n2);
 
+//Función
 int main()
 {
     //Variables locales
+    /*Se declaran dentro de una función, mientras 
+    que la variable global se declara fuera de la función.
+    Las variables locales se crean cuando la funcion
+    ha iniciado la ejecucion, se pierde cuando 
+    la funcion termina*/
     int n1, n2;
     cout << "Digite primer numero: ";
     cin >> n1;
